548ef40fbfe0f463ce17ad6830388169104c8a80
venkat ravilla <venkatravilla333@gmail.com>
2025-09-19 16:47:15 +0530

    initial commit

---
+++ 1.Basics/index.js
@@ -0,0 +1,58 @@
+
+// console.log(a)
+var a = 'sachin'
+console.log(a, typeof a)
+
+var b = 40
+console.log(b, typeof b)
+
+var c = true
+console.log(c, typeof c)
+
+var d
+console.log(d, typeof d)
+
+var e = null
+console.log(e, typeof e)
+
+var f = {
+  name: 'sachin',
+  age: 40
+}
+console.log(f, typeof f)
+// console.log(f.name)
+// console.log(f.age)
+
+var g = [1, 2, 3]
+console.log(g, typeof g)
+// console.log(g[0])
+// console.log(g[1])
+// console.log(g[2])
+
+// var x = 100
+// function test() {
+//   var y = 200
+// }
+// console.log(test, typeof test)
+// test()
+
+// var h = function hello() {
+  
+// }
+// console.log(h)
+// var i = function () {
+  
+// }
+// console.log(i)
+// var j =  ()=> {
+  
+// }
+// console.log(j)
+
+// function test() {
+//   var x = 100
+//   return x
+// }
+
+// console.log(test())
+  
