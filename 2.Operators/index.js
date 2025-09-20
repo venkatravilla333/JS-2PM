3b77837b4a4c80f683fccfa6e127e1ee303cffa1
venkat ravilla <venkatravilla333@gmail.com>
2025-09-20 16:31:02 +0530

    operators

---
+++ 2.Operators/index.js
@@ -0,0 +1,107 @@
+
+
+var a = 10
+
+var b = 20
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a ** b)
+
+// console.log(a)
+// console.log(++a)
+// console.log(++a)
+// console.log(a++)
+// console.log(a)
+// console.log(--a)
+// console.log(a--)
+// console.log(a)
+
+
+// assignment
+
+// var a = 10
+// console.log(a)
+
+// console.log(a += 90)
+// console.log(a -= 90)
+// console.log(a *= 90)
+// console.log(a /= 90)
+// console.log(a %= 90)
+// console.log(a **= 90)
+
+//compariosion
+
+// var a = 10
+// var b = 20
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a < b)
+// console.log(a <= b)
+
+//equality
+
+// var a = 10
+// var b = 20
+// var b = 10
+// var b = '10'
+
+// var a = undefined
+// var b = null
+
+// var a = {}
+// var b = {}
+// var a = {value: 100}
+// var b = {value: 100}
+// var b = a
+
+// var a = 10
+
+// var b = {}
+
+// var a = ()=>{}
+// // var b = ()=>{}
+// var b = a
+
+// var a = [1, 2, 3]
+// // var b = [1, 2, 3]
+// var b = a
+
+// var a = 10
+// var b = 10
+
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a != b)
+// console.log(a !== b)
+
+
+//ternary operator
+
+// var age = 20
+
+// var res = age >= 18 ? 'having vote right' : 'no vote right'
+// console.log(res)
+
+//logical
+
+// var income = false
+// var cibil = true
+// var localAddress = false
+
+// var ICICICC = income && cibil && localAddress
+// var RBLCC = income || cibil && localAddress
+// console.log(RBLCC)
+
+// var res = !income
+// console.log(!res)
+
+
+
+
+
+
+
