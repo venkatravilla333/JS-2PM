552f7860f28fec847fc4a06715ce54bf332dd2e1
venkat ravilla <venkatravilla333@gmail.com>
2025-09-22 16:45:00 +0530

    control statements

---
+++ 3.Control-Statements/index.js
@@ -0,0 +1,134 @@
+
+
+// var num = 0
+
+// if (num > 0) {
+//   console.log('positive number')
+// } else if (num < 0) {
+//   console.log('negative number')
+// } else {
+//   console.log('Zero')
+// }
+
+// var num = 4
+
+// switch (num) {
+//   case num > 0:
+//     console.log('positive number')
+//     break;
+//   case num < 0:
+//     console.log('negative number')
+//     break;
+//   default:
+//     console.log('Zero')
+// }
+
+// var marks = 101
+
+// if (marks < 35) {
+//   console.log('fail')
+// } else if (marks >= 35 && marks < 60) {
+//   console.log('second class')
+// } else if (marks >= 60 && marks < 75) {
+//   console.log('first class')
+// } else if (marks >= 75 && marks <= 100) {
+//   console.log('distinction')
+// } else {
+//   console.log('invalid marks')
+// }
+
+// var marks = 80
+
+// switch (marks) {
+//   case marks < 35:
+//     console.log('fail')
+//     break;
+//   case marks >= 35 && marks < 60:
+//     console.log('second class')
+//     break;
+//   case marks >= 60 && marks < 75:
+//     console.log('first class')
+//     break;
+//   case marks >= 75 && marks <= 100:
+//     console.log('distinction')
+//     break;
+//   default:
+//     console.log('invalid marks')
+// }
+
+// var weekDay = 8
+
+// if (weekDay === 1) {
+//   console.log('monday')
+// } else if (weekDay === 2) {
+//   console.log('theusday')
+// } else if (weekDay === 3) {
+//   console.log('wednesday')
+// } else if (weekDay === 4) {
+//   console.log('thursday')
+// } else if (weekDay === 5) {
+//   console.log('friday')
+// } else if (weekDay === 6) {
+//   console.log('saturday')
+// } else if (weekDay === 7) {
+//   console.log('sunday')
+// } else {
+//   console.log('invalid day')
+// }
+
+// var weekDay = 8
+
+// switch (weekDay) {
+//   case 1:
+//     console.log('monday')
+//     break;
+//   case 2:
+//     console.log('tuesday')
+//     break;
+//   case 3:
+//     console.log('wednesday')
+//     break;
+//   case 4:
+//     console.log('thursday')
+//     break;
+//   case 5:
+//     console.log('friday')
+//     break;
+//   case 6:
+//     console.log('saturday')
+//     break;
+//   case 7:
+//     console.log('sunday')
+//     break;
+//   default:
+//     console.log('invalid day')
+// }
+
+
+// var customer = 'mongo'
+
+// if (customer === 'Gold') {
+//   console.log('gold customer')
+// } else if (customer === 'Silver') {
+//   console.log('silver customer')
+// } else if (customer === 'Iron') {
+//   console.log('iron customer')
+// } else {
+//   console.log('Invalid customer')
+// }
+
+// var customer = 'mongo'
+
+// switch (customer) {
+//   case 'Gold':
+//     console.log('Gold customer')
+//     break;
+//   case 'Silver':
+//     console.log('silver customer')
+//     break;
+//   case 'Iron':
+//     console.log('iron customer')
+//     break;
+//   default:
+//     console.log('invalid customer')
+// }
