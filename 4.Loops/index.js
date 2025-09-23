c3a8a866baa71c349951523977853910e3deb3c8
venkat ravilla <venkatravilla333@gmail.com>
2025-09-23 16:43:50 +0530

    loops

---
+++ 4.Loops/index.js
@@ -0,0 +1,213 @@
+
+// var name = 'sachin'
+
+//without using loop
+
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+
+
+//with using loop
+
+// for (var i = 1; i <= 100; i++){
+//   // if(i === 50) continue
+//   // if(i === 60) continue
+//   console.log(i, name)
+// }
+
+// var i = 1
+
+// while (i <= 100) {
+//   console.log(i, name)
+//   i++
+// }
+
+// var i = 101
+
+// do {
+//   console.log(i, name)
+//   i++
+// }while(i<=100)
+
+
+
+
+// var arr = [1, 2, 3, 4, 5]
+
+// console.log(arr)
+
+// without loop
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// var iteratorObj = arr[Symbol.iterator]()
+// console.log(iteratorObj)
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+
+//ES-6
+
+// for (var value of arr) {
+//   console.log(value)
+// }
+
+// ES-5
+
+// for (var i = 0; i < arr.length; i++){
+//   console.log(i, arr[i])
+// }
+
+
+// var obj = {
+//   name: 'sachin',
+//   age: 40,
+//   city: 'mumbai'
+// }
+
+
+// var arr = [1, 2, 3, 4, 5]
+
+//without using loop
+
+// console.log(obj.name)
+// console.log(obj.age)
+// console.log(obj.city)
+
+// for (var value of obj) {
+//   console.log(value)
+// }
+
+// for (var index in arr) {
+//   console.log(index, arr[index])
+// }
+
+// for (var keys in obj) {
+//   console.log(keys, obj[keys])
+// }
+
+// var msg = 'hi how are you'
+
+// console.log(msg.length)
+
+// for (var value of msg) {
+//   console.log(value)
+// }
+// for (var i = 0; i < msg.length; i++) {
+//   console.log(i, msg[i])
+// }
+
+
+
+
+
+
+
+
+
