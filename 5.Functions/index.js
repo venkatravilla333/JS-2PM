39cf83fb9c181e7c1ae9b7a9b8dbd116b84bab4c
venkat ravilla <venkatravilla333@gmail.com>
2025-09-24 16:21:58 +0530

    functions class-1

---
+++ 5.Functions/index.js
@@ -0,0 +1,75 @@
+
+
+// function test() {
+  
+// }
+// test()
+
+// //expression
+
+// var x = function test() {
+  
+// }
+// x()
+
+// var y = function () {
+  
+// }
+// y()
+
+// var z = ()=> {
+  
+// }
+// z()
+
+//params vs arguments
+
+// function test(a = 10,b = 20,c = undefined) {
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+// }
+
+// test(1,true, undefined)
+
+
+
+// varying no of params vs arguments
+
+// var test = ()=> {
+//   // console.log(a + b + c)
+//   console.log(arguments, Array.isArray(arguments))
+//   console.log(arguments[0])
+//   console.log(arguments[1])
+//   console.log(arguments[2])
+//   console.log(arguments[3])
+//   console.log(arguments[4])
+
+//   var sum = 0
+
+//   for (var value of arguments) {
+//     console.log(value)
+//     sum += value
+//   }
+//   console.log(sum)
+// }
+// test(1, 2, 3, 4, 5)
+
+
+// var test = (...rest)=> {
+//   console.log(rest)
+//   // console.log(rest[0])
+//   // console.log(rest[1])
+//   // console.log(rest[2])
+//   // console.log(rest[3])
+//   // console.log(rest[4])
+//   var sum = 0
+//   for (var value of rest) {
+//     sum += value
+//   }
+//   console.log(sum)
+// }
+// test( 1, 2, 3, 4, 5)
+
+
+ 
