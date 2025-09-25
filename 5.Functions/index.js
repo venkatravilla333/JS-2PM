5b3e2f383f3c6c86c5ea3864d60d56e17954db27
venkat ravilla <venkatravilla333@gmail.com>
2025-09-25 15:13:27 +0530

    scope & hoisting

--- 5.Functions/index.js
+++ 5.Functions/index.js
@@ -72,4 +72,108 @@
 // test( 1, 2, 3, 4, 5)
 
 
- 
+//scope
+
+// var a = 1
+
+// let b = 2
+
+// const c = 3
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// function test() {
+
+//   var d = 4
+//   let e = 5
+//   const f = 6
+
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log(d)
+//   console.log(e)
+//   console.log(f)
+//   if (true) {
+//     var g = 7
+//     let h = 8
+//     const i = 9
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+//     console.log(d)
+//     console.log(e)
+//     console.log(f)
+//     console.log(g)
+//     console.log(h)
+//     console.log(i)
+//   }
+//   console.log(g)
+//   // console.log(h)
+//   // console.log(i)
+// }
+
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+// test()
+console.log(a)
+let a = 'sachin'
+console.log(a)
+
+console.log(b)
+var b = 40
+console.log(b)
+
+console.log(c)
+var c = true
+console.log(c)
+
+console.log(d)
+var d
+console.log(d)
+
+console.log(e)
+var e = null
+console.log(e)
+
+console.log(f)
+var f = {
+  name: 'kohli'
+}
+console.log(f)
+
+console.log(g)
+var g = [1, 2, 3]
+console.log(g)
+
+console.log(h)
+var h = function named() {
+  console.log('named')
+}
+h()
+console.log(h)
+
+console.log(i)
+var i = function () {
+  console.log('ananymous')
+}
+console.log(i)
+
+console.log(j)
+var j = () => {
+  console.log('arrow')
+}
+console.log(j)
+
+console.log(declare)
+declare()
+function declare() {
+  console.log('declare')
+}
+console.log(declare)
