e136fb10438513ef34bc67a3ef7b527190a2a1e4
venkat ravilla <venkatravilla333@gmail.com>
2025-11-07 17:02:24 +0530

    generator function & regular function vs arrow function

---
+++ 17.Generator-functions/index.js
@@ -0,0 +1,29 @@
+
+
+// // function test() {
+// //   return 100
+// // }
+
+// // console.log(test())
+
+// function* genFun() {
+//   try {
+//     yield 100
+    
+//   } finally {
+//     yield 200
+//   }
+ 
+// }
+
+// var genObj = genFun()
+// // console.log(genObj)
+// // console.log(genObj.next())
+// // console.log(genObj.return())
+// // console.log(genObj.next())
+
+// for (var value of genObj) {
+//   console.log(value)
+// }
+
+
